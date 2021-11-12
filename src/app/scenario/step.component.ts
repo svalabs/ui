c67b629029dfadb18f970ce15e12730d575f1fd8
Raphael von der Grün <raphael.vondergruen@sva.de>
2021-11-12 17:08:14 +0100

    Remove undeclared dependency on lodash

--- src/app/scenario/step.component.ts
+++ src/app/scenario/step.component.ts
@@ -25,9 +25,10 @@ import { VMInfoConfig } from '../VMInfoConfig';
 import { environment } from 'src/environments/environment';
 import { ShellService } from '../services/shell.service';
 import { atou } from '../unicode';
-import { escape } from 'lodash';
 import { SplitAreaDirective, SplitComponent } from "angular-split";
 
+// Replacement for lodash's escape
+const escape = (s: string) => s.replace(/[&<>"']/g, c => `&#${c.charCodeAt(0)};`);
 
 @Component({
     templateUrl: 'step.component.html',

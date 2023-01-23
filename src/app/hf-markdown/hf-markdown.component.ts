37e70ca893ebbf6764fe7fb59aad5e92c07a9895
Jan-Gerrit Goebel <Jan-Gerrit.Goebel@sva.de>
2023-01-23 14:38:06 +0100

    Remove log

--- src/app/hf-markdown/hf-markdown.component.ts
+++ src/app/hf-markdown/hf-markdown.component.ts
@@ -167,7 +167,6 @@ export class HfMarkdownComponent implements OnChanges {
   }
 
   private replaceVmInfoTokens(content: string) {
-    console.log(this.context.vmInfo);
     return content.replace(
       /\$\{vminfo:([^:]*):([^}]*)\}/g,
       (match, vmName, propName) => {
